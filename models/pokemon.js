a42fd412042d14f3d9cd4b9bcf526925c1c2510f
cb2z <cb2z@fresnoU.org>
2025-03-12 09:07:28 -0700

    Pokemon3

--- models/pokemon.js
+++ models/pokemon.js
@@ -16,24 +16,28 @@ module.exports = (sequelize, DataTypes) => {
   Pokemon.init({
     name: DataTypes.STRING,
     health: DataTypes.STRING,
-    type: DataTypes,
-    type2,
-    pokemon_image,
-    move_type,
-    second_move_type,
-    skill,
-    damage,
-    move_type2,
-    second_move_type2,
-    skill2,
-    damage2,
-    weakness,
-    resist,
-    retreat,
-    retreat_cost
+    type: DataTypes.STRING,
+    type2: DataTypes.STRING,
+    pokemon_image: DataTypes.STRING,
+    move_type :DataTypes.STRING,
+    second_move_type: DataTypes.STRING,
+    skill: DataTypes.STRING,
+    damage:DataTypes.INTEGER,
+    move_type2: DataTypes.STRING,
+    second_move_type2:DataTypes.STRING,
+    skill2:DataTypes.STRING,
+    damage2:DataTypes.INTEGER,
+    weakness:DataTypes.STRING,
+    resist:DataTypes.STRING,
+    retreat:DataTypes.STRING,
+    retreat_cost:DataTypes.INTEGER,
+    ptype: DataTypes.STRING,
+    ptype2: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Pokemon',
+    tableName: 'pokemons',
+    timestamps: false,
   });
   return Pokemon;
 };
