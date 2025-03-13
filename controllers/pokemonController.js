9db698c1958c7d1186b2fa461c88e4f1f59310c3
cb2z <cb2z@fresnoU.org>
2025-03-13 09:11:47 -0700

    Pokemon4

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -1,7 +1,14 @@
    var express = require('express');
 var router = express.Router();
 const {Pokemon} = require('../models');
+const types =['electric', 'water', 'ghost', 'normal', 'fire', 'grass']
 module.exports.viewAll = async function(req, res, next) {
     const pokemons = await Pokemon.findAll();
     res.render('index', {pokemons});
+}
+module.exports.renderEditForm = async function(req, res, next) {
+    const pokemon = await Pokemon.findBypk(
+        req.params.id
+    );
+    res.render('edit', {pokemon, types});
 }
